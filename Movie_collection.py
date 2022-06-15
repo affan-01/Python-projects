1894775e6c4774b7310970a7187eb6388ac8ad5b
Mohammed Affan <60263481+affan-01@users.noreply.github.com>
2022-06-15 19:35:46 +0530

    Update Movie_collection.py
    added comments

--- Movie_collection.py
+++ Movie_collection.py
@@ -1,18 +1,23 @@
+#Program to answer user queries on Movie collection
+
 PROMPT = "Press 'A' to add movie to list , Press 'L' to print the list of movies" \
          "Press 'F' to find movies Or Press 'Q' to end menu: "
 
 movie_list = []
 
+#function to add a movie to the list
 def add_movie(name, director):
     movie_list.append({
         'movie_name' : name,
         'director_name': director
     })
 
+#function to print movie name and the director name
 def print_list():
     for i in movie_list:
         print(f"title: {i['movie_name']} director_name: {i['director_name']}")
 
+#function to check if a movie exists in the list
 def find_movie(name):
     for i in movie_list:
         if i['movie_name'] == name:
@@ -23,6 +28,7 @@ def find_movie(name):
 
 selection = input(PROMPT).lower()
 
+#Main user menu loop
 while selection != 'q':
     if selection == 'a':
         movie_title = input("Type in movie title: \n")
